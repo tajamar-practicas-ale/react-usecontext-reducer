474a69b171ada7d8597e50dfb6798a1da6348cdd
alexanderrjstj <alexanderpaolo.rojas@tajamar365.com>
2025-04-30 20:45:36 +0200

    fix all 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
-    <App />
-  </CartProvider>
+  <StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </StrictMode>,
 )
