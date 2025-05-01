52cbf9bf66fa6b58c2e75165cc31c9d745090f29
alexanderrjstj <alexanderpaolo.rojas@tajamar365.com>
2025-05-01 10:38:57 +0200

    prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        asda
+        asdaasdadasdas
 
       </p>
       {
